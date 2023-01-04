e087524f7d9b344e22d69485d026cee7a1ae5f8a
William Kelly <wimikelly@gmail.com>
2023-01-04 13:28:00 -0500

    Starting material UI init to little avail

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useRef, useState } from 'react';
 import './App.css';
 import jsonTiles from "./map.json";
+import ToggleButtons from "./components/ToggleButtons";
+
 
 
 
@@ -8,19 +10,20 @@ import jsonTiles from "./map.json";
 function App() {
 
   const [tiles, setTiles] = useState(jsonTiles);
+
   
-const handleOnClick = (tile) => {
-  console.log(tile);
-  let tempTiles = tiles;
-  tempTiles[tile.key].color = "#000000";
-  setTiles([...tempTiles]);
-}
+  const handleOnClick = (tile) => {
+    console.log(tile);
+    let tempTiles = tiles;
+    tempTiles[tile.key].color = "#000000";
+    setTiles([...tempTiles]);
+  }
 
   return (
     <div className="App">
-      <header>
-        <h1>commons</h1>
-      </header>
+
+<ToggleButtons/>
+
 
       <div className="Grid">
           {tiles.map((tile, i) => <div onClick={() => handleOnClick(tile)} key={tile.key} style={{background: tile.color}} className="tile"></div>)}
