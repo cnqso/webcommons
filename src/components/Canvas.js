15258f19c2b652c76abc4fd6a346d93eb8b981d0
William Kelly <wimikelly@gmail.com>
2023-01-05 17:56:53 -0500

    Improved performance and made grid size configurable

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { FixedSizeGrid as Grid } from 'react-window';
 import './Canvas.css';
+import { Space, Pressable, PressEventCoordinates } from 'react-zoomable-ui';
+import config from './config';
  
 
  
@@ -26,36 +28,86 @@ const useWindowSize = () => {
     return windowSize;
   };
 
+
+
+
+
+
+
 const Canvas = (props) => {
+
+    const TILE_WIDTH = 60;
+    const TILE_HEIGHT = 60;
+    const TILE_PIXELS = 8;
+
+
+    const tiles = props.tiles;
     const { width, height } = useWindowSize();
     const square = (width + height) / 60;
+    const imageWidth = config.TILE_WIDTH * config.TILE_PIXELS;
+    const imageHeight = config.TILE_HEIGHT*config.TILE_PIXELS;
 
-    const Cell = ({ columnIndex, rowIndex, style }) => (
-        <div 
-            onClick={() => props.handleOnClick(columnIndex, rowIndex)} 
-            
-            style={style}><div className={"tile"} style={{background: props.tiles[columnIndex+(rowIndex*120)].color}}> 
-          </div>
-        </div>
-      );
+    //const [hover, setHover] = useState();
+    const clickxy = useRef([0,0]);
 
+    // const SimpleTapCountingButton = React.memo(() => {
+    //   const [tapCount, setTapCount] = React.useState(0);
+    //   const [message, setMessage] = React.useState('TAP ME');
+    //   return (
+    //     <Pressable
+    //       className={'prez'}
+    //       potentialTapStyle={{ backgroundColor: 'blue' }}
+    //       potentialLongTapStyle={{ backgroundColor: 'darkblue' }}
+    //       hoverStyle={{ backgroundColor: 'orchid' }}
+    //       onTap={() => {
+    //         props.handleOnClick(0,0)
+    //       }}
+    //       onLongTap={() => {
+    //         setMessage('LONG TAPPED!');
+    //       }}
+    //     >
+    //       {message}
+    //     </Pressable>
+    //   );
+    // });
 
-
-
-    console.log(props)
-    return (
-  <Grid
-    columnCount={120}
-    columnWidth={square}
-    height={height/1.2}
-    rowCount={100}
-    rowHeight={square}
-    width={width}
-    className={"Grid"}
     
-  >
-    {Cell}
-  </Grid>
+    // const Tilemap = React.memo(() => {
+    //   return (
+    //     <Pressable className={'Grid'} onTap={() => {props.handleOnClick(clickxy.current)}}>
+    //     {tiles.map((tile, i) => <div key={i} style={{background: tile.color}} className="tile"></div>)}
+    //     </Pressable>
+    //   );
+    // });
+
+
+
+
+    return (
+      <div style={{ width: width, height: (height/1.2), position: "relative" }}>
+        <Space
+        onDecideHowToHandlePress={(e, coords) => {clickxy.current = [coords.x, coords.y]}}
+        //onHover={(e, c) => setHover(c)}
+          style={{ border: "solid 1px black" }}
+          onCreate={viewPort => {
+            viewPort.setBounds({ x: [0, imageWidth], y: [0, imageHeight] });
+            viewPort.camera.centerFitAreaIntoView({
+              left: 0,
+              top: 0,
+              width: imageWidth,
+              height: imageHeight
+            });
+          }}
+        >  
+
+        {/* <div className="Grid">
+            {tiles.map((tile, i) => <div key={i} style={{background: tile.color}} className="tile"></div>)}
+        </div> */}
+        <Pressable style={{gridTemplateColumns: `repeat(${config.TILE_WIDTH}, 8px)`,  width: `${imageWidth}`, height: `${imageHeight}`}} className={'Grid'} onTap={() => {props.handleOnClick(clickxy.current)}}>
+        {tiles.map((tile, i) => <div key={i} style={{background: tile.color}} className="tile"></div>)}
+        </Pressable>
+        </Space>
+      </div>
     )
     };
 
---
+++ src/components/config.js
@@ -0,0 +1,5 @@
+export default {
+    TILE_WIDTH: 60,
+    TILE_HEIGHT: 60,
+    TILE_PIXELS: 8,
+}
